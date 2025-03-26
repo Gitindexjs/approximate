54ed77ff939a7700f02c9188932e3a8512450887
gitindex <38837497+Gitindexjs@users.noreply.github.com>
2025-03-26 16:22:34 +0000

    Update kt.js

--- kt.js
+++ kt.js
@@ -11,9 +11,18 @@ let c = 1;
 grid[2][2] = c
 
 function pPos(p){
-    for i in range(2):
-        for j in range(2):
-            for k in range(2):
+    let possibilities = []
+    for i in range(2){
+        for j in range(2){
+            for k in range(2){
                 add = [((i+1)%2)*(((j+1)%2)*2-1)+(i%2)*(((k+1)%2)*5-2), (i%2)*(((j+1)%2)*2-1)+((i+1)%2)*(((k+1)%2)*5-2)]
-                return p.map((x,i) => x + add[i])
+                possibilities.push(p.map((x,i) => x + add[i]))
+            }
+        }
+   }
+    for i in range(len(possibilities)){
+        if(possibilities[i][0] < 0 || possibilities[0] > total-1 || possibilities[1] < 0 || possibilities[1] > total -1){
+            possibilities.splice(i, 1)       
+        }
+    }
 }
